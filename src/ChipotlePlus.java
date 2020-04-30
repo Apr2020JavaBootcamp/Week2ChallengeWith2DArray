5eb093a08bceeed0bfc7b4edb4914f42b435584b
mbake94 <mbake94@gmail.com>
2020-04-30 07:39:43 -0700

    completed

--- src/ChipotlePlus.java
+++ src/ChipotlePlus.java
@@ -4,45 +4,42 @@ import java.util.Random;
 public class ChipotlePlus {
     public static void main(String[] args) {
         double cost = 3.00;
-        double totalCost = 0;
+        //double totalCost = 0;
         String[] item;
         String[] burritoNum = {"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th"};
         String[][] toppings = {
-                {"white", "brown", "none", "all"}, // rice [0]
-                {"chicken", "steak", "carnidas", "chorizo", "sofritas", "veggie meat", "none", "all"}, //meat [1]
-                {"pinto", "black", "none", "all"}, // beans [2]
-                {"mild", "medium", "hot", "none", "all"}, // salsa [3]
-                {"lettuce", "fajita veggies", "none", "all"}, // veggies [4]
-                {"yes", "no"}, // cheese [5]
-                {"yes", "no"}, //guacamole [6]
-                {"yes", "no"}, //queso [7]
-                {"yes", "no"}, //sour scream [8]
+                {"white rice", "brown rice", "no rice", "all rice"}, // rice [0]
+                {"chicken", "steak", "carnidas", "chorizo", "sofritas", "veggie meat", "no meat", "all meat"}, //meat [1]
+                {"pinto beans", "black beans", "no beans", "all beans"}, // beans [2]
+                {"mild salsa", "medium salsa", "hot salsa", "no salsa", "all salsa"}, // salsa [3]
+                {"lettuce", "fajita veggies", "no veggies", "all"}, // veggies [4]
+                {"cheese", "no cheese"}, // cheese [5]
+                {"guac", "no guac"}, //guacamole [6]
+                {"queso", "no queso"}, //queso [7]
+                {"sour cream", "no sour cream"}, //sour scream [8]
         };
 
         for (int i = 0; i < burritoNum.length; i++) {
-            System.out.printf("Your %s burrito costs $%.2f, and has: ", burritoNum[i], cost+3.00);
-
+           System.out.printf("Your %s burrito contains: ", burritoNum[i]);
 
             for (int j = 0; j < toppings.length; j++) {
                 item = toppings[j];
-                double index = (double) (Math.random() * item.length);
-                double all = index * 0.5;
+                int index = (int) (Math.random() * item.length);
+               // double all = index * 0.5;
                 double none = 0;
-                if (toppings.equals("all")) {
-                    cost = all;
-                } else if (toppings.equals("none")) {
-                    cost = none;
+
+                if (item[index].contains("no")) {
+                    cost += 0;
                 } else {
-                    cost = index + 3;
+                    cost += 0.5;
                 }
 
-                System.out.print(item[(int)index] + ", ");
+               System.out.print((item[index]) + ", ");
 
             }
-
+            System.out.printf("Cost: $%.2f",cost);
             System.out.println("\n");
-
+            cost = 3;
         }
-
     }
 }
